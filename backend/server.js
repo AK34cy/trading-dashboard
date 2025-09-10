59f883ecf0a70817e4721e5eccfcd62d03180635
AK34cy <khodunov@gmail.com>
2025-09-10 18:42:13 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
+import positionsRouter from "./routes/positions.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// маршруты
+app.use("/positions", positionsRouter);
+
+// базовый маршрут для проверки
+app.get("/", (req, res) => {
+  res.send("Backend работает!");
+});
+
+// запуск сервера
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
+});
