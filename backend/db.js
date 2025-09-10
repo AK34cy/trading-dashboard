d616fbe4df43ce48b6fd7cf772bd6f8903582702
AK34cy <khodunov@gmail.com>
2025-09-10 19:12:14 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,14 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+import dotenv from 'dotenv';
+dotenv.config();
 
+const pool = new Pool({
+  user: process.env.DB_USER || 'devuser',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'trading_db',
+  password: process.env.DB_PASSWORD || 'seggaq-qijxaw-1cawNu',
+  port: process.env.DB_PORT || 5432,
+});
+
+export default pool;
