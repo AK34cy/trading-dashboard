f4cedca69bde52f076e4de9fa6a48bfccf66a9d9
AK34cy <khodunov@gmail.com>
2025-09-10 17:01:40 +0300

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,1 +1,36 @@
+// базовый URL для API
+const API_URL = "http://163.5.63.244:5000";
 
+// функция для GET-запросов
+export async function getRequest(endpoint) {
+  try {
+    const response = await fetch(`${API_URL}${endpoint}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Ошибка запроса GET:", error);
+    throw error;
+  }
+}
+
+// функция для POST-запросов
+export async function postRequest(endpoint, data) {
+  try {
+    const response = await fetch(`${API_URL}${endpoint}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Ошибка запроса POST:", error);
+    throw error;
+  }
+}
