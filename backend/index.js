233f5d70c8bc17f3e81150ab35ed98fab3b49dcc
AK34cy <khodunov@gmail.com>
2025-09-13 11:43:36 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-// backend/index.js
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -11,44 +10,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middlewares
+// middlewares
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// routes
 app.use("/positions", positionsRouter);
-console.log("positionsRouter загружен");
-
 app.use("/users", usersRouter);
-console.log("usersRouter загружен");
-
 app.use("/auth", authRouter);
-console.log("authRouter загружен");
 
-// Root
-app.get("/", (req, res) => {
-  res.send("Backend is running");
-});
+// root
+app.get("/", (req, res) => res.send("Backend работает!"));
 
-// Безопасное логирование маршрутов
-setImmediate(() => {
-  if (app._router && app._router.stack) {
-    console.log("Содержимое app._router.stack:");
-    app._router.stack.forEach((layer, idx) => {
-      if (layer.route) {
-        console.log(`${idx}: ROUTE path=${layer.route.path}, methods=${Object.keys(layer.route.methods)}`);
-      } else if (layer.name === "router") {
-        console.log(`${idx}: ROUTER path=`, layer.regexp);
-      } else {
-        console.log(`${idx}: MIDDLEWARE name=${layer.name}`);
-      }
-    });
-  } else {
-    console.log("Маршруты ещё недоступны (_router undefined)");
-  }
-});
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`Backend running on port ${PORT}`);
-});
+// запуск сервера
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
