f672c69ba549defc8acd0e63b3df5a2d7663e04b
AK34cy <khodunov@gmail.com>
2025-09-13 12:05:00 +0300

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,14 +1,17 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+  const token = authHeader && authHeader.split(" ")[1]; // Bearer <token>
 
-  if (!token) return res.status(401).json({ error: "Нет токена" });
+  if (!token) return res.status(401).json({ error: "Токен не предоставлен" });
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ error: "Токен недействителен" });
-    req.user = user;
+    if (err) return res.status(403).json({ error: "Неверный токен" });
+    req.user = user; // сохраняем данные пользователя в объект запроса
     next();
   });
 };
